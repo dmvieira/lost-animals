e7d07a01b60f5a90440db1342bea176127fc80dc
Diogo <diogo.mvieira@labsynapse.com>
2015-03-29 17:08:23 -0300

    static now work

--- lostanimals/lostpet/views.py
+++ lostanimals/lostpet/views.py
@@ -7,12 +7,14 @@ def index(request):
     context = dict(jedi='sou sim')
     return render(request, 'lostpet/index.html', context)
 
+
 def new(request):
-    form = PetForm()
     if request.method == 'POST':
         formset = PetForm(request.POST, request.FILES)
         if formset.is_valid():
             formset.save()
     else:
         formset = PetForm()
+    print formset
+    print dir(formset)
     return render(request, 'lostpet/new.html', dict(form=formset))
